eec1a8a95ab4323f36caac740a62e2abd0b06962
Jamie Little <jamie.littledv@gmail.com>
2024-12-07 12:23:43 +0000

    Resolving PR comments

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const server = express();
 
 // Initialise the web server
@@ -7,10 +6,12 @@ server.get('/', (req, res) => {
     res.send('Hello from Inspector API')
 })
 
+// Listener for /helloworld endpoint
 server.get('/helloworld', (req, res) => {
     res.send('Hello World - first initial dev')
 })
 
+// Start the webserver, listening on port 4040 (may change later)
 server.listen(4040, () => {
     console.log('Express server started...')
 })
