ac14ea40873d8c224f5c375c59841d6de7bfd05a
jamielitt-guitar <jamie.little2@rvu.co.uk>
2024-12-13 15:08:25 +0000

    Added push endpoint to parse urls out of json submission (#4)
    * Added push endpoint to parse urls out of json submission
    
    * Removed debug stuff
    
    ---------
    
    Co-authored-by: Jamie Little <LITTLEJ2@confused.com>

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const server = express();
+var jsonParser = bodyParser.json();
 
 // Initialise the web server
 server.get('/', (req, res) => {
@@ -9,6 +12,18 @@ server.get('/', (req, res) => {
 // Listener for /helloworld endpoint
 server.get('/helloworld', (req, res) => {
     res.send('Hello World - first initial dev')
+})
+
+// Endpoint for submitting a json request
+server.post('/inspecturls', jsonParser, (req, res) => {
+    
+    // Convert boduy to JSON
+    var inspector = req.body;
+
+    // Output the urls to console window for now just to confirm they've been received
+    inspector.urls.forEach(element => {
+        console.log(element)
+    });
 })
 
 // Start the webserver, listening on port 4040 (may change later)
